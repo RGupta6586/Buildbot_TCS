1dd027ea4ec8ad642cf48ea9b3b63f7771e577d8
RGupta6586 <44025467+RGupta6586@users.noreply.github.com>
2018-10-11 13:21:19 +0530

    Update test.py

--- hello/test.py
+++ hello/test.py
@@ -1,1 +1,17 @@
-print ("Hello")
+
+"""sample test"""
+         self.assertEqual(hello('world'), 'hello world')
+
+     def test_world_bytes(self):
+         """sample test with bytes"""
+         self.assertEqual(hello(b'world'), b'hello world')
+
+     def test_world_unicode(self):
+         """sample test with unicode"""
+         self.assertEqual(hello(u'world'), u'hello world')
+          
+          
+
+     def test_world_bytes(self):
+         """sample test with bytes"""
+         self.assertEqual(hello(b'bot'), b'hello bot')
