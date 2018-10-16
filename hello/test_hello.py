50c2621fb71fb268394633219cef293f83da6a0a
pratikjhaveri <jhaveri.pratik@tcs.com>
2018-10-17 00:55:41 +0530

    [MR:3] changed hello file

---
+++ hello/test_hello.py
@@ -0,0 +1,16 @@
+"""sample test"""
+import unittest
+
+from hello import hello
+
+
+class TestHello(unittest.TestCase):
+    """sample test"""
+
+    def test_world(self):
+        """sample test"""
+        self.assertEqual(hello('world1'), 'hello world1')
+
+    def test_world_unicode(self):
+"""sample test with unicode"""
+        self.assertEqual(hello(u'world'), u'hello world')
