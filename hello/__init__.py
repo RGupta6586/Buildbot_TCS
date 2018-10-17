9c3510b48456915bce25c1776189208b2d92df61
pratikjhaveri <jhaveri.pratik@tcs.com>
2018-10-18 00:15:11 +0530

    [MR:10] Addition of new function and python file

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,3 +4,8 @@
 def hello(who):
     """function that greats"""
     return "hello " + who
+
+
+def test(tcs):
+     """TCS test function"""
+     return 5
---
+++ hello/test_tcs.py
@@ -0,0 +1,14 @@
+"""sample test"""
+import unittest
+
+from hello import tcs
+
+
+class NewTest(unittest.TestCase):
+    """sample test"""
+
+    def sum(self):
+        """sample test"""
+        a = 2;
+        b = 3;
+        self.assertEqual(tcs, (a+b))
