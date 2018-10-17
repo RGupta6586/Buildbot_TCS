74a1e6feebd1ecc2204dcb5a5319f139732f3f55
pratikjhaveri <jhaveri.pratik@tcs.com>
2018-10-18 02:53:33 +0530

    [MR:15] test hello file has deleted

--- tcs/test_tcs.py
+++ tcs/test_tcs.py
@@ -8,7 +8,7 @@ class NewTest(unittest.TestCase):
     """sample 465651 test"""
 
     def sum(self):
-        """sample test"""
+        """sample add test"""
         a = 2;
         b = 3;
         self.assertEqual(tcs(a+b), 5)
